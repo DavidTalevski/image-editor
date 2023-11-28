080929317e00eac71ad191b9239527ffce33c65e
David Talevski <david.talevski@x3mlabs.com>
2023-11-28 22:55:28 +0100

    Add ImageDownloader

--- image-editor/src/js/core/downloader/imageDownloader.js
+++ image-editor/src/js/core/downloader/imageDownloader.js
@@ -1,14 +1,31 @@
-export class ImageDownloader {
-    constructor() {
+import { ImageType } from "../enum/imageType.enum";
 
+export class ImageDownloader {
+
+    /** @private */
+    TAG = "[Image Downloader]";
+
+    constructor(canvas) {
+        this.canvas = canvas;
     }
 
-    download() {
-        const canvas = canvasRef.current;
-        const image = canvas.toDataURL('image/png');
+    /**
+     * @param {string} fileName 
+     * @param {(typeof ImageType)[keyof typeof ImageType]} type
+     * @param {number} quality - For JPEG images, specify the quality (0 to 1)
+     */
+    download(fileName, type, quality = 1) {
+        if (!fileName || fileName === "") fileName = "image";
+        if (quality < 0 || quality > 1) quality = 1;
+
+        const name = `${fileName}.${type}`;
+        const image = this.canvas.toDataURL(`image/${type}`, quality);
         const link = document.createElement('a');
+
         link.href = image;
-        link.download = 'canvas_image.png';
+        link.download = name;
+
+        console.log(this.TAG, "Downloading image:", name);
         link.click();
     }
 }
--- image-editor/src/js/core/enum/imageType.enum.js
+++ image-editor/src/js/core/enum/imageType.enum.js
@@ -1,6 +1,6 @@
 /** @enum {string} */
-export const ImageType = {
+export const ImageType = Object.freeze({
     PNG: "png",
     WEBP: "webp",
     JPEG: "jpeg"
-}
+})
