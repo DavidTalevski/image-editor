f787db2b6d879e09d90b99774e8ae12c9259816a
David Talevski <david.talevski@x3mlabs.com>
2023-11-29 22:45:11 +0100

    Added separate functions for loading, added clipboard image loading

--- image-editor/src/js/core/loader/imageLoader.js
+++ image-editor/src/js/core/loader/imageLoader.js
@@ -5,37 +5,59 @@ export class ImageLoader {
     /** @protected */
     TAG = "[Image Loader]";
 
+    static Error = ImageLoaderError;
+
     /**
-     * @param {Blob} file 
+     * @param {string} url 
      * @returns {Promise<HTMLImageElement>}
      */
-    load(blob) {
-        return new Promise(async (resolve, reject) => {
-            let fileData;
-
-            try {
-                fileData = await this.read(blob)
-            } catch (e) {
-                return reject(e);
-            }
-
-            const image = new Image();
-
-            image.onerror = (e) => {
-                console.log(this.TAG, "Failed loading image:", e);
-                reject(ImageLoaderError.FAILED_LOADING_FILE);
-            }
-
-            image.onload = () => {
-                console.log(this.TAG, "Successfully loaded image")
-                resolve(image)
-            };
-
-            image.src = fileData;
-        })
+    loadFromUrl(url) {
+        return this.load(url);
     }
 
     /**
+     * @param {Blob} blob
+     * @returns {Promise<HTMLImageElement>}
+     */
+    async loadFromBlob(blob) {
+        let fileData;
+
+        try {
+            fileData = await this.read(blob)
+        } catch (e) {
+            throw e;
+        }
+
+        return this.load(fileData);
+    }
+
+    /**
+     * @returns {Promise<HTMLImageElement>}
+     */
+    async loadFromClipboard() {
+        let data;
+
+        try {
+            data = await navigator.clipboard.read();
+        } catch (e) {
+            throw ImageLoader.Error.FAILED_READING_CLIPBOARD;
+        }
+
+        if (!data) throw ImageLoader.Error.NO_IMAGE_COPIED_IN_CLIPBOARD;
+
+        for (const item of data) {
+            if (item.types.includes("image/png")) {
+                const blob = await item.getType("image/png");
+                return this.loadFromBlob(blob);
+            }
+        }
+
+        throw ImageLoader.Error.NO_IMAGE_COPIED_IN_CLIPBOARD;
+    }
+
+
+    /**
+     * @private
      * @param {Blob} blob 
      * @returns {Promise<String>}
      */
@@ -45,7 +67,7 @@ export class ImageLoader {
 
             reader.onerror = (e) => {
                 console.log(this.TAG, "Failed reading file:", e);
-                reject(ImageLoaderError.FAILED_READING_FILE);
+                reject(ImageLoader.Error.FAILED_READING_FILE);
             }
 
             reader.onload = (e) => {
@@ -54,6 +76,29 @@ export class ImageLoader {
             }
 
             reader.readAsDataURL(blob);
+        })
+    }
+
+    /**
+     * @private
+     * @param {any} data 
+     * @returns {Promise<HTMLImageElement>}
+     */
+    load(data) {
+        return new Promise(async (resolve, reject) => {
+            const image = new Image();
+
+            image.onerror = (e) => {
+                console.log(this.TAG, "Failed loading image:", e);
+                reject(ImageLoader.Error.FAILED_LOADING_FILE);
+            }
+
+            image.onload = () => {
+                console.log(this.TAG, "Successfully loaded image")
+                resolve(image)
+            };
+
+            image.src = data;
         })
     }
 
