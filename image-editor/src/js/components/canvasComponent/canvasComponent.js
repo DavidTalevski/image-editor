83a43927b16d206b1cd747f235f6b1eb1e8bed1b
David Talevski <davidtalevski00@gmail.com>
2024-01-20 17:48:43 +0100

    Fixed canvas size and removed border

--- image-editor/src/js/components/canvasComponent/canvasComponent.js
+++ image-editor/src/js/components/canvasComponent/canvasComponent.js
@@ -13,7 +13,6 @@ const CanvasComponent = React.forwardRef((props, ref) => {
             }}
             width={"100%"}
             height={"100%"}
-            className="canvas-container"
             style={{
                 position: "relative",
                 top: 0,
